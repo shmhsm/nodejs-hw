51659bcdeddcb9fc43aa351f1c0708699baad769
shmhsm <shm.ughh@gmail.com>
2026-06-24 19:33:18 +0200

    fix: add trim to title/content and set default tag to Todo in Note model

--- src/models/note.js
+++ src/models/note.js
@@ -6,14 +6,17 @@ const noteSchema = new Schema(
     title: {
       type: String,
       required: true,
+      trim: true, 
     },
     content: {
       type: String,
       default: '',
+      trim: true, 
     },
     tag: {
       type: String,
       enum: TAGS,
+      default: 'Todo', 
     },
   },
   {
